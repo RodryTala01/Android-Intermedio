3351fdb3075a4b67c3c5b9d05950dcf4e0efbdcd
RodryTala01 <rodrytalarico@gmail.com>
2024-05-20 20:10:15 -0300

    initial commit

---
+++ app/src/main/java/com/rodrytala01/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,24 @@
+package com.rodrytala01.horoscapp.ui.luck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rodrytala01.horoscapp.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
